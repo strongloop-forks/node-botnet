2e0c2f69a278bfb44ff56b4914c5af6ba38bd95a
Ryan Dahl <ry@tinyclouds.org>
2010-12-18 15:39:28 -0800

    Add test

---
+++ test/test-simple-server-client.js
@@ -0,0 +1,38 @@
+var botnet = require('../lib/botnet');
+var assert = require('assert');
+
+var gotMessage = false;
+var clientBot;
+
+var serverBot = botnet.createBot(__dirname + '/keys/agent2-keys');
+serverBot.listen(8123);
+
+serverBot.on('message', function (message, peer) {
+  console.error("msg: %j", message);
+  assert.equal('hello world', message.msg);
+  gotMessage = true;
+
+  serverBot.close();
+  clientBot.close();
+});
+
+
+serverBot.on('peerConnect', function (peer) {
+  console.log("connection: %d\n%j", peer.sessionId, peer.cert);
+});
+
+
+serverBot.on('listening', function () {
+  clientBot = botnet.createBot(__dirname + '/keys/agent1-keys');
+
+  clientBot.connect(8123, function (peer) {
+    console.log("connected: %d\n%j", peer.sessionId, peer.cert);
+    peer.send({ msg: "hello world" });
+    console.error("message sent");
+  });
+});
+
+
+process.on('exit', function () {
+  assert.ok(gotMessage);
+});
