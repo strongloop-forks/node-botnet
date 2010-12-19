bdaf12265dea0927607e75c2b8d03565444d44fc
Ryan Dahl <ry@tinyclouds.org>
2010-12-19 14:09:27 -0800

    All bots should start a server

--- test/test-simple-server-client.js
+++ test/test-simple-server-client.js
@@ -5,17 +5,31 @@ var gotMessage = false;
 var clientBot;
 
 var serverBot = botnet.createBot(__dirname + '/keys/agent2-keys');
-serverBot.listen(8123);
 
 serverBot.on('message', function (message, peer) {
   console.error("msg: %j", message);
   assert.equal('hello world', message.msg);
   gotMessage = true;
 
+  /*
   serverBot.close();
   clientBot.close();
+  */
+
 });
 
+
+setTimeout(function () {
+  var cs = clientBot._state();
+  var ss = serverBot._state();
+  console.log(cs);
+  console.log(ss);
+
+  assert.deepEqual(cs, ss);
+
+  serverBot.close();
+  clientBot.close();
+}, 500);
 
 serverBot.on('peerConnect', function (peer) {
   console.log("connection: %d\n%j", peer.sessionId, peer.cert);
