30d820ed82b9dae7232b1b23b2adfcde32b5a108
Ryan Dahl <ry@tinyclouds.org>
2010-12-20 19:10:48 -0800

    add shell upgrade code

--- examples/simple/client.js
+++ examples/simple/client.js
@@ -1,8 +1,30 @@
 var botnet = require('../../lib/botnet');
 var bot = botnet.createBot(__dirname + '../../../test/keys/agent1-keys');
 
-bot.connect(8123, function (peer) {
+bot.connect(function (peer) {
   console.error("connected: %d", peer.sessionId);
   bot.broadcast({ msg: "hello world" });
   console.error("message sent");
+
+  peer.shell(function (connection, firstChunk) {
+    console.error("Opening shell!");
+
+    // Set rawMode ?
+    require('tty').setRawMode(true);
+
+    // XXX why do i have to test here? should be able to write empty buffer.
+    if (firstChunk && firstChunk.length) {
+      process.stdout.write(firstChunk);
+    }
+    connection.pipe(process.stdout);
+    var stdin = process.openStdin();
+    stdin.pipe(connection);
+
+    stdin.on('close', function () {
+      console.error("stdin close");
+      bot.close();
+      require('tty').setRawMode(false);
+    });
+
+  });
 });
--- examples/simple/server.js
+++ examples/simple/server.js
@@ -1,13 +1,11 @@
 var botnet = require('../../lib/botnet');
 var bot = botnet.createBot(__dirname + '../../../test/keys/agent2-keys');
 
-bot.listen(8123);
-
 bot.on('message', function (message, peer) {
   console.error("msg: %j", message);
 });
 
 
 bot.on('peerConnect', function (peer) {
-  console.log("connection: %d\n%j", peer.sessionId, peer.cert);
+  console.error("someone connected");
 });
