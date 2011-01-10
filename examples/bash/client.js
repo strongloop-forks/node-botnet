7f112ca338cd7b6ae8a241269083f1a319b82c04
Ryan Dahl <ry@tinyclouds.org>
2011-01-10 14:50:56 -0800

    shell improvements

--- examples/bash/client.js
+++ examples/bash/client.js
@@ -4,8 +4,6 @@ var bot = botnet.createBot(__dirname + '../../../test/keys/agent1-keys');
 bot.connect(function (err, peer) {
   if (err) throw err;
   console.error("connected: %d", peer.sessionId);
-  bot.broadcast({ msg: "hello world" });
-  console.error("message sent");
 
   peer.shell(function (connection, firstChunk) {
     console.error("Opening shell!");
@@ -17,14 +15,20 @@ bot.connect(function (err, peer) {
     if (firstChunk && firstChunk.length) {
       process.stdout.write(firstChunk);
     }
+
     connection.pipe(process.stdout);
     var stdin = process.openStdin();
     stdin.pipe(connection);
 
     stdin.on('close', function () {
-      console.error("stdin close");
-      bot.close();
       require('tty').setRawMode(false);
+    });
+
+    connection.on('close', function () {
+      require('tty').setRawMode(false);
+      console.error("\r\nConnection closed.");
+      bot.close();
+      stdin.destroy();
     });
 
   });
