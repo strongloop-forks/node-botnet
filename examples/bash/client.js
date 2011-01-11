b2c92e0f7a41f2f6271d06b1fec1d9a5177e7043
Ryan Dahl <ry@tinyclouds.org>
2011-01-10 18:03:33 -0800

    More advanced shell features
    requires node b9cfd9527acad0a686083078863bff028dfd785a or better.

--- examples/bash/client.js
+++ examples/bash/client.js
@@ -1,31 +1,48 @@
 var botnet = require('../../lib/botnet');
 var bot = botnet.createBot(__dirname + '../../../test/keys/agent1-keys');
+var tty = require('tty');
+
+function sendWindowSize(peer) {
+  peer.shellSendWindowSize(0);
+}
+
 
 bot.connect(function (err, peer) {
   if (err) throw err;
   console.error("connected: %d", peer.sessionId);
 
+  process.on('SIGWINCH', function() {
+    sendWindowSize(peer);
+  });
+
   peer.shell(function (connection, firstChunk) {
     console.error("Opening shell!");
 
     // Set rawMode ?
-    require('tty').setRawMode(true);
+    tty.setRawMode(true);
 
     // XXX why do i have to test here? should be able to write empty buffer.
     if (firstChunk && firstChunk.length) {
       process.stdout.write(firstChunk);
     }
+    sendWindowSize(peer);
 
     connection.pipe(process.stdout);
-    var stdin = process.openStdin();
+    var stdin = process.stdin;
+    stdin.resume();
     stdin.pipe(connection);
 
     stdin.on('close', function () {
-      require('tty').setRawMode(false);
+      tty.setRawMode(false);
+        /*
+      if (connection.writable) {
+        peer.send({ cmd: 'shellClose' });
+      }
+        */
     });
 
     connection.on('close', function () {
-      require('tty').setRawMode(false);
+      tty.setRawMode(false);
       console.error("\r\nConnection closed.");
       bot.close();
       stdin.destroy();
